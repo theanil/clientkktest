899bd35f14a6038de6233627847f146ed9b03691
theanil <theanil@yahoo.com>
2016-06-10 21:35:13 +0530

    444

--- js/services.js
+++ js/services.js
@@ -858,7 +858,7 @@ $(document).on('pageinit', '#beforelogin', function()
 								//localStorage.setItem("session_id_username", username);
 								localStorage.setItem("session_id_local", session_id);
 								//localStorage.setItem("session_name", urldecode(result.data.name));
-								//localStorage.setItem("session_id_mu_id", mu_id);
+								localStorage.setItem("session_id_mu_id", mu_id);
 								//localStorage.setItem("session_id_club_id", selected_club);
 								//localStorage.setItem("session_contact_details_id", contact_details_id);
 
