d9afa16e976e172f010b5eb2325b5f4114543df4
theanil <theanil@yahoo.com>
2016-06-11 12:22:03 +0530

    uuu

--- js/services.js
+++ js/services.js
@@ -857,12 +857,12 @@ $(document).on('pageinit', '#beforelogin', function()
 								//return false;
 								//localStorage.setItem("session_id_username", username);
 								localStorage.setItem("session_id_local", session_id);
-								//localStorage.setItem("session_name", urldecode(result.data.name));
+								localStorage.setItem("session_name", urldecode(result.data.name));
 								localStorage.setItem("session_id_mu_id", mu_id);
 								//localStorage.setItem("session_id_club_id", "1");
 								localStorage.setItem("session_id_club_id", selected_club);
-								//localStorage.setItem("session_contact_details_id", contact_details_id);
-								//localStorage.setItem("session_club_info", club_info);
+								localStorage.setItem("session_contact_details_id", contact_details_id);
+								localStorage.setItem("session_club_info", club_info);
 								GetIDDetailsInfo();
 								//GetBuildingWingInfo();
 																
